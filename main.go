924e9c92808c9b5df0f5bfab626e40e990d5e0be
Ibtisam Mirza <iuhmirza@gmail.com>
2023-05-25 15:44:57 +0100

    Add form for long link

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ func test(res http.ResponseWriter, req *http.Request) {
 	log.Println("test called")
 	log.Println(req.Header)
 	log.Println(req.Body)
+	log.Println(req.Method)
+	log.Println(req.FormValue("longlink"))
 }
 
 func main() {
 	log.Println("main called")
 
+	var fs http.Handler = http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/submit", test)
+
 	http.HandleFunc("/test/", test)
-	http.Handle("/google", http.RedirectHandler("http://www.google.com", 301))
+	http.Handle("/google", http.RedirectHandler("http://www.google.com", http.StatusMovedPermanently))
 	log.Println("route(s) handled")
 
 	err := http.ListenAndServe(":3000", nil)
