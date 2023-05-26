e29923886e2fbb7db2e7f3a738434c7841da7e50
Ibtisam Mirza <iuhmirza@gmail.com>
2023-05-26 11:36:36 +0100

    Refactor template into separate file

--- response.go
+++ response.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-const SHORT_LINK_PAGE_TEMPLATE string = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
-</head>
-<body>
-    <code>localhost:3000/{{.}}</code>
-</body>
-</html>
-`
+var linkedPageTemplate = filepath.Join("templates", "linked.html")
+var linkedPage = template.Must(template.ParseFiles(linkedPageTemplate))
 
-var SHORT_LINK_PAGE *template.Template
-
-func init() {
-	SHORT_LINK_PAGE, _ = template.New("short_page").Parse(SHORT_LINK_PAGE_TEMPLATE)
-}
-
-func GenerateResponse(res http.ResponseWriter, short string) {
-	SHORT_LINK_PAGE.Execute(res, short)
+func WriteLinked(res http.ResponseWriter, short string) {
+	res.Header().Set("Content-Type", "text/html")
+	err := linkedPage.Execute(res, short)
+	if err != nil {
+		log.Println(err)
+	}
 }
